e9a08a3863b46e43a59bc0c472e34cf968899d4d
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2022-10-18 21:48:33 -0400

    Init

---
+++ MyReportCard/MyReportCard.Business/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MyReportCard.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyReportCard/MyReportCard.Data/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MyReportCard.Data
+{
+    public class Class1
+    {
+    }
+}
